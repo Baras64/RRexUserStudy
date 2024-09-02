ad7b762e2706cd3c382c17132ff711012472c8d8
Harshavardhan <harsh@Harshavardhans-Laptop.local>
2024-09-02 20:29:29 +0800

    combining tutorial 1 and 2

--- tutorial.js
+++ tutorial.js
@@ -56,5 +56,25 @@ if(tutorialId === '1'){
 
     quesTitle.style.display = 'none';
     containerInfo.style.display = 'none';
+} else if (tutorialId === '12') {
+    quesTitle.style.display = 'none';
+    // table.style.display 
+    legend.style.display = 'none';
+    // containerInfo.style.display = 'none';
+    temp = setInterval(() => {
+        // console.log("hey")
+        if(table.rows.length > 0){
+            rows = table.rows;
+            rows[0].style.display = "none";
+            rows[1].style.display = "none";
+            for (let i = 2; i < rows.length; i++) {
+                cells = rows[i].cells;
+                for (let j = 3; j < cells.length; j++) {
+                    cells[j].style.display = "none";
+                }
+            }
+            clearInterval(temp);
+        }
+    }, 100);
 }
 
